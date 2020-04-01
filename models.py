Correct dim. of rbf weights & nca loss computation

models.py
@@ -17,7 +17,7 @@ class Classifier(tf.keras.Model):
                                          input_shape=(params['image_size'], params['image_size'], 3),
                                          pooling='avg')
         self.feat_dim = self.feature_extractor.output_shape[-1]
-        self.fcw = tf.keras.layers.Dense(1, kernel_initializer='he_normal', activation='sigmoid', use_bias=False)
+        self.fcw = tf.keras.layers.Dense(params['num_neighbors']-1, kernel_initializer='he_normal', activation='sigmoid', use_bias=False)
 
     def call(self, inputs, training=None, mask=None):
         features = self.feature_extractor(inputs)
@@ -26,7 +26,8 @@ class Classifier(tf.keras.Model):
         if self.params['use_weights']:
             w = self.fcw(features)
             out["w"] = w
-
+        else:
+            out["w"] = tf.ones_like(self.params['num_neighbors']-1)
         return out
 
 

train.py
@@ -11,7 +11,6 @@ parser.add_argument("--train_dir", default='./imgs/bns', type=str)
 parser.add_argument("--num_samples", default=20, type=int)
 parser.add_argument("--image_size", default=224, type=int)
 parser.add_argument("--embedding_dim", default=2048, type=int)
-parser.add_argument("--num_classes", default=2, type=int)
 parser.add_argument("--num_neighbors", default=5, type=int)
 parser.add_argument("--global_scale", default=20.0, type=float)
 parser.add_argument("--num_epochs", default=6, type=int)
@@ -60,7 +59,7 @@ def update_centres(model, init_ds, storage, epoch, k=5):
     return storage, neighbor_ids
 
 
-def nca_loss(data, model, optimizer, storage, neighbor_ids, args):
+def train_step(data, model, optimizer, storage, neighbor_ids, args):
     # data
     images, labels, ids = data
 
@@ -68,11 +67,7 @@ def nca_loss(data, model, optimizer, storage, neighbor_ids, args):
         # forward pass
         outs = model(images, training=True)
         outs_z = outs["z"]
-
-        if args['use_weights']:
-            outs_w = outs["w"]
-        else:
-            outs_w = tf.ones_like(labels)
+        outs_w = outs["w"]
 
         # get indices, centres, labels of nearest neighbors
         nn_ids = neighbor_ids[ids.numpy()][:, 1:]  # exclude input sample itself from neighbor list
@@ -81,9 +76,8 @@ def nca_loss(data, model, optimizer, storage, neighbor_ids, args):
 
         # compute nca loss
         loss = 0.0
-        for i, (out_z, l, nn_c, nn_l) in enumerate(zip(outs_z, labels, nn_centres, nn_labels)):
-            p = get_class_probs(out_z, nn_c, nn_l, outs_w, args)
-            loss += loss_fn(p, l, args['num_classes'])
+        for i, (out_z, l, nn_c, nn_l, out_w) in enumerate(zip(outs_z, labels, nn_centres, nn_labels, outs_w)):
+            loss += get_nca_loss(out_z, l, nn_c, nn_l, out_w, args)
 
     gradients = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
@@ -102,7 +96,7 @@ if __name__ == "__main__":
     init_ds = get_dataset(args['train_dir'], batch_size=args['batch_size'], epochs=1, shuffle=False)
     storage = {
         'centres': np.zeros((args['num_samples'], args['embedding_dim']), dtype=np.float32),
-        'labels': np.zeros((args['num_samples'],), dtype=np.int32),
+        'labels': np.zeros((args['num_samples']), dtype=np.int32),
     }
     neighbor_ids = np.zeros((args['num_samples'], args['num_neighbors']), dtype=np.int32)
 
@@ -122,10 +116,10 @@ if __name__ == "__main__":
         # Update gaussian centres & k-nearest neighbor list
         if step == 0 or step % c_update_interval_iter == 0:
             storage, neighbor_ids = update_centres(model, init_ds, storage, epoch, k=args['num_neighbors'])
-            print("Updating centres & neighbor list..\n".format(epoch))
+            print("Updating centres & neighbor list..".format(epoch))
 
-        # Compute nca loss
-        loss = nca_loss(data, model, optimizer, storage, neighbor_ids, args)
+        # Train step
+        loss = train_step(data, model, optimizer, storage, neighbor_ids, args)
 
         print('Epoch: [{}/{}], Loss: {:.4f}'.format(epoch + 1, args['num_epochs'], loss))
 

utils.py
@@ -1,51 +1,35 @@
 import tensorflow as tf
 
 
-def get_class_probs(x, c, l, w, params):
+def get_nca_loss(x, l, nn_c, nn_l, w, params):
     """
-    TODO: vectorize over l
-    Return class probabilities for a single training example, based on the embedding vector and a vector of gaussian kernel
-    centres c, one for each training sample, except the current sample
-    labels: vector with labels associated with class centres c
-    w: weights of each sample
-    Dim(c) = (m - 1, embedding_dimension)
-    Dim(l) = (m - 1,)
-    Dim(w) = (m - 1,)
+    Return NCA loss for a single training example, based on the embedding vector, and its nearest neighbours
+    (i.e., vectors of gaussian kernel centres, except the current sample).
+    x: embedding vector of single training sample
+    l: class label associated with embedding vector
+    nn_c: vectors of gaussian kernel centres (nearest neighbours of x)
+    nn_l: class labels associated with nearest neighbours
+    w: weights of training sample
+    Dim(c) = (k - 1, embedding_dimension)
+    Dim(l) = (k - 1,)
+    Dim(w) = (k - 1,)
     Dim(x) = (embedding_dimension,)
-    Dim(x) = (batch_size, embedding_dimension) -> not yet implemented
     """
-    # print(x.shape)
-    # print(c.shape)
-    # print(l.shape)
-    # print(w.shape)
     assert x.shape == (params['embedding_dim'],)
-    assert c.shape[0] == l.shape[0]
+    assert nn_c.shape[0] == nn_l.shape[0]
 
-    # if w is None:
-    #     w = tf.ones_like(l)
+    # class probability
+    nn_w = tf.expand_dims(w, axis=-1)   # Dim(nn_w) = (k - 1, 1)
+    denominator = tf.reduce_sum(nn_w * (tf.exp(-((x - nn_c) ** 2) / (2 * params['global_scale'] ** 2))))
 
-    # p = tf.zeros((params.num_classes,))
+    c_match = nn_c[l.numpy() == nn_l]
+    w_match = nn_w[l.numpy() == nn_l]
+    enumerator = tf.reduce_sum(w_match * (tf.exp(-((x - c_match) ** 2) / (2 * params['global_scale'] ** 2))))
 
-    denominator = tf.reduce_sum(w * (tf.exp(-((x - c) ** 2) / (2 * params['global_scale'] ** 2))))
-    p = []
-    for class_i in range(params['num_classes']):
-        c_match = c[l == class_i]
-        w_match = w[l == class_i]
-        enumerator = tf.reduce_sum(w_match * (tf.exp(-((x - c_match) ** 2) / (2 * params['global_scale'] ** 2))))
-        p.append(enumerator / denominator)
-
-    p = tf.concat([p], axis=0)
-
+    p = enumerator/denominator
     p += 1e-6  # for numeric stability
 
-    return p
+    # NCA loss
+    loss = -tf.math.log(p)
 
-
-def loss_fn(p, t, num_classes):
-    """
-    Loss of predictions
-    dim(p): (num_classes,)
-    t: integer, index of p
-    """
-    assert p.shape == (num_classes,)
-    return -tf.math.log(p[t])
+    return loss